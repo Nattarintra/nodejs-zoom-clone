6cd631d08f1b2e50ca53912056c8b217bd5409ee
Natta <nattarintra.thutsanti@hotmail.com>
2021-08-14 22:19:23 +0200

    Add function for vedio call and answer the call

--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
-
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
+var peer = new Peer(undefined, {
+  path: '/peerjs', // from sever.js
+  host: '/',
+  port: '3030'
+});
 let myVideoStream 
 navigator.mediaDevices.getUserMedia({
   video: true,
@@ -10,7 +15,49 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream =>{
   myVideoStream = stream;
   addVideoStream(myVideo, stream);
+
+  peer.on('call', call => {
+    // when B call me I answered the call
+    call.answer(stream) // I'm gonna answer B call
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      // I'm gonna add video stream from B user 
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', (userId) =>{
+    connectToNewUser(userId, stream);
+  })
+
+  
 })
+
+// Listen to peer connections
+ peer.on('open', id =>{
+ // console.log(id);
+
+ // Join room ID from room.ejs file
+socket.emit('join-room', ROOM_ID, id);
+
+ });
+
+
+const connectToNewUser = (userId, stream) =>{
+  //console.log(userId);
+  // Call another user that joined the room
+  const call = peer.call(userId, stream) // call B user and I'm gonna send my stream.
+  const video = document.createElement('video') // I'm gonna create new video element for B
+  call.on('stream', userVideoStream => { // when I receive stream, then I will add video stream.
+    addVideoStream(video, userVideoStream)
+  })
+  // call.on('close', () => {
+  //   video.remove()
+  // })
+
+  //peers[userId] = call
+}
+
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 const {v4: uuidv4} = require('uuid');
+
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -13,4 +20,15 @@ app.get('/:room', (req, res) =>{
   res.render('room',{roomId: req.params.room});
 })
 
+io.on('connection',socket => {
+  // Gonna join that room
+  socket.on('join-room', (roomId,userId)=>{
+    //console.log('Joined room');
+    socket.join(roomId); // now join the room
+    socket.broadcast.to(roomId).emit('user-connected', userId); 
+    //socket.to(roomId).broadcast.emit('user-connected'); // not working
+   // socket.to(roomId).emit('user-connected'); // alternative 
+    
+  })
+})
 server.listen(3030);
